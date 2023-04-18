0b8a3444694466369e8fad82fe1c7c82ee3ff147
Josh <kcchang1994@gmail.com>
2023-04-18 12:33:53 +0800

    新增輸入數字、符號功能

--- src/components/calculatorType_IOS/InnerComponents/ButtonSection.tsx
+++ src/components/calculatorType_IOS/InnerComponents/ButtonSection.tsx
@@ -1,4 +1,4 @@
-import { useState, SetStateAction, Dispatch } from 'react'
+import { useState, SetStateAction, Dispatch, KeyboardEvent, useEffect } from 'react'
 import CalculateButton from './CalculateButton'
 import NumberButton from './NumberButton'
 import './ButtonSection.scss'
@@ -8,51 +8,89 @@ function ButtonSection(props: propsType) {
     const [oprateSign,setOprateSign] = useState('')
     const [saveOprateSign,setSaveOprateSign] = useState('')
 
-    const handleCalculatorClick = (event: React.MouseEvent<Element>) => {
-        const eventTarget = event.target as HTMLElement
-        if(!eventTarget) return
+    const handleKeydownEvent = (event: any) => {
+        // 判斷是否為數字
+        const NumberArray = ['1','2','3','4','5','6','7','8','9','0']
+        let isNumber = false
+        NumberArray.forEach((number)=> {
+            if(number == event.key) {
+                isNumber = true
+                return 
+            }
+        })
+        if(isNumber) {
+            return handleCalculatorClick(null,event.key,'number')
+        }
+        // 判斷是否為符號
+        const operaterArray = ['+','-','Enter','.','*','/','%']
+        const operaterCorrespondArray = ['plus','minus','equal','addDecimal','multiply','division','percent']
+        let isOperater = false
+        operaterArray.forEach((operater)=> {
+            if(operater == event.key) {
+                isOperater = true
+                return 
+            }
+        })
+        if(isOperater) {
+            const index = operaterArray.indexOf(event.key)
+            return handleCalculatorClick(null,  operaterCorrespondArray[index],'operater')
+        }
+    }
+
+    const addKeyInListener = () => {
+        window.addEventListener('keydown',handleKeydownEvent)
+    }
+
+    useEffect(()=> {
+        addKeyInListener()
+        return () => {
+            removeEventListener('keydown', handleKeydownEvent);
+        };
+    },[addKeyInListener,props.count,props.calCount])
+
+    const handleCalculatorClick = (event: (React.MouseEvent<Element>) | null, eventKey: string, triggerFrom: string) => {
+        const eventTarget = event?.target as HTMLElement
+        console.log((eventKey && triggerFrom === 'operater'))
         if( 
-            eventTarget.classList.contains('numberBtnStyle') && 
-            eventTarget.dataset.value
+            (eventKey && triggerFrom === 'number') ||
+            (eventTarget && eventTarget.classList.contains('numberBtnStyle') && eventTarget.dataset.value)
         ) {
             if(saveOprateSign) {
                 const calCountValue = props.calCount ? props.calCount.toString() : 0
-                const newInput: string = eventTarget.dataset.value
+                const newInput: string =  eventKey || eventTarget.dataset.value || ''
                 const newCalCount: string = Number(calCountValue.toString() + newInput).toString()
                 setOprateSign('')
                 props.setCalCount(newCalCount)
             } else {
-                const newInput: string = eventTarget.dataset.value
+                const newInput: string = eventKey || eventTarget.dataset.value || ''
                 let newCount: string 
                 if(props.count === '0' || props.calCount ==='0') {
                     newCount = newInput
                 } else {
                     newCount = (props.count + newInput)
                 }
-                
-                
                 props.setCount(newCount) 
             }
+            return
         } 
-    
-        if(
-            eventTarget.classList.contains('calBtnStyle') && 
-            eventTarget.dataset.value
+        else if(
+            (eventKey && triggerFrom === 'operater') ||
+            (eventTarget && eventTarget.classList.contains('calBtnStyle') && eventTarget.dataset.value)
         ) {
-            const newSign = eventTarget.dataset.value
+            const newSign = eventKey || eventTarget.dataset.value || ''
             if(oprateSign === newSign) return 
             else {
                 if(newSign === 'addDecimal') {
-                const nowNum = props.calCount === null ? props.count : props.calCount
-                if(!nowNum.toString().includes('.')){
-                    props.calCount === null ? props.setCount(props.count + '.') : props.setCalCount(props.calCount + '.')
-                }
-                return
+                    const nowNum = props.calCount === null ? props.count : props.calCount
+                    if(!nowNum.toString().includes('.')){
+                        props.calCount === null ? props.setCount(props.count + '.') : props.setCalCount(props.calCount + '.')
+                    }
+                    return
                 }
                 if(newSign === 'percent') {
-                const newPercentNum = props.calCount === null ? (Number(props.count) / 100).toString() : (Number(props.calCount) / 100).toString()
-                props.calCount === null ? props.setCount(newPercentNum) : props.setCalCount(newPercentNum)
-                return
+                    const newPercentNum = props.calCount === null ? (Number(props.count) / 100).toString() : (Number(props.calCount) / 100).toString()
+                    props.calCount === null ? props.setCount(newPercentNum) : props.setCalCount(newPercentNum)
+                    return
                 }
                 calculateValue()
                 if(newSign === 'equal') {
@@ -106,7 +144,7 @@ function ButtonSection(props: propsType) {
     }
 
     return (
-        <section className='controlsStyle' onClick={handleCalculatorClick}>
+        <section className='controlsStyle' onClick={(event) => handleCalculatorClick(event, '','')}>
             <CalculateButton 
                 showText="AC" 
                 value="deleteAll"
