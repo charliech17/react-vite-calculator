239f88d9b4b05a8403aa6742e8f5839ed961f18b
Josh <kcchang1994@gmail.com>
2023-12-07 15:31:48 +0800

    將style改成module

--- src/components/calculatorType_IOS/IosCalculator.tsx
+++ src/components/calculatorType_IOS/IosCalculator.tsx
@@ -1,14 +1,14 @@
 import { useState, memo } from 'react'
 import ButtonSection from './InnerComponents/ButtonSection'
-import './IosCalculator.scss'
+import styles from './IosCalculator.module.scss'
 
 function IosCalculator() {
     const [count, setCount] = useState('0')
     const [calCount,setCalCount] = useState<null | string>(null)
     
     return (
-        <div className='calculatorWrapper'>
-            <section className='displayStyle'>
+        <div className={styles.calculatorWrapper}>
+            <section className={styles.displayStyle}>
                 <div>{calCount === null ? count : calCount }</div>
             </section>
             <ButtonSection 
